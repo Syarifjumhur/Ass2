3760710eaaf41625e112c8085db6cb7bad5b359e
Syarifjumhur <160558949+Syarifjumhur@users.noreply.github.com>
2024-05-06 12:01:44 +0700

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -1,35 +1,29 @@
 <?php
-
 // Koneksi ke database
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'penjualan_obat';
-$koneksi = new mysqli($host, $username, $password, $database);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Penjualan_obat";
 
-// Periksa koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi ke database error: " . $conn->connect_error);
 }
 
-// Query untuk mendapatkan data dari tabel penjualan_obat
-$query = "SELECT * FROM penjualan_obat";
-$result = $koneksi->query($query);
+//Query untuk mendapatkan semua data lowongan
+$sql = "SELECT * FROM penjualan";
+$result = $conn->query($sql);
 
-// Buat array untuk menampung data
 $data = array();
-
-// Ambil data dari hasil query dan tambahkan ke dalam array
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $data[] = $row;
     }
 }
 
-// Tutup koneksi
-$koneksi->close();
-
-// Mengembalikan data dalam bentuk JSON
+//Mengembalikan data dalam format JSON
 echo json_encode($data);
 
+$conn->close();
 ?>
